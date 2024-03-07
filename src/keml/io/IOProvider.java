9ad4c7b6886339cba9c9725a35e2422fb8d21509
Susanne Göbel <susanne.goebel@buildup.ch>
2024-03-07 08:01:42 +0100

    Added model save, currently on main should be moved into own module

--- src/keml/io/IOProvider.java
+++ src/keml/io/IOProvider.java
@@ -4,8 +4,15 @@
 package keml.io;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import keml.Conversation;
+import keml.KemlPackage;
 import keml.io.graphml.GraphML2KEML;
 
 /**
@@ -23,9 +30,17 @@ public class IOProvider {
 		System.out.println("Current dir:" + currentPath);
 		GraphML2KEML myReader = new GraphML2KEML();
 		Conversation conv = myReader.readFromPath("../../graphs/objective3-2-2v4.graphml");
-		System.out.println(conv.getTitle());
-	
+		//  write to file		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		ResourceSetImpl resSet = new ResourceSetImpl();
+		resSet.getPackageRegistry().put(KemlPackage.eNS_URI, KemlPackage.eINSTANCE);
+		resSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("keml", new XMIResourceFactoryImpl());
+		
+		Resource resource = resSet.createResource(URI.createFileURI("../../graphs/objective3-2-2v4.keml"));
+		resource.getContents().add(conv);
 
+		resource.save(null);
+		
 	}
 
 }

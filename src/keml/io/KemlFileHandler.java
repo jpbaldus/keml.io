9b8e9f045711820403ea50e1ca12bd2bd81bf135
Susanne Göbel <goebel@uni-koblenz.de>
2024-06-24 09:35:33 +0200

    Replace library version, now vendor changed to eclipse cloud as well

--- src/keml/io/KemlFileHandler.java
+++ src/keml/io/KemlFileHandler.java
@@ -7,7 +7,7 @@ import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.emfjson.jackson.resource.JsonResourceFactory;
+import org.eclipse.emfcloud.jackson.resource.JsonResourceFactory;
 
 import keml.Conversation;
 import keml.KemlPackage;
